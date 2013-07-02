1313ea621adbb4ddcbfca7e3e2a3187af2a4d446
Casey Foster <c@sey.me>
2013-07-02 16:06:35 -0500

    Redirect to specific hosts

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.enable('strict routing');
 // Don't waste bytes on an extra header.
 app.disable('x-powered-by');
 
+// Redirect to hosts specified in config.
+app.use(function (req, res, next) {
+  var proto = req.protocol;
+  var path = req.path.replace(/\/+/g, '/').replace(/\/$/, '') || '/';
+  var query = req.url.replace(/^[^?]*/, '');
+  var current = proto + '://' + req.host + req.url;
+  var host =
+    ~_.indexOf(config.hosts, req.host) ?
+    req.host :
+    config.hosts[Math.floor(Math.random() * config.hosts.length)];
+  var correct = (proto + '://' + host + path).toLowerCase() + query;
+  if (current === correct) return next();
+  res.redirect(correct, 301);
+});
+
 // Requests should be in the form /path/to/image(-job-name).extension
 app.get(new RegExp(config.pattern), function (req, res, next) {
   var base = req.params[0];
