490716f0d310fd5e84e0b7fa999fabc0eb3cebfb
Casey Foster <c@sey.me>
2013-07-02 17:56:04 -0500

    Add support for putting images in s3

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
 var _ = require('underscore');
 var config = require(process.argv[2] || './crop');
+var crypto = require('crypto');
 var express = require('express');
+var fs = require('fs');
 var gm = require('gm');
+var http = require('http');
+var knox = require('knox');
+var mime = require('mime');
 var request = require('request');
 
 var app = express();
+
 app.listen(config.port);
-
-// Express will be running behind nginx, so let it know.
-app.enable('trust proxy');
-
-// All lower case and no trailing slashes allowed.
-app.enable('case sensitive routing');
-app.enable('strict routing');
 
 // Don't waste bytes on an extra header.
 app.disable('x-powered-by');
 
-// Redirect to hosts specified in config.
+// Parse the request body for POSTing files.
+app.use(express.bodyParser());
+
+var s3 = knox.createClient({
+  key: config.accessKeyId,
+  secret: config.secretAccessKey,
+  bucket: config.bucket
+});
+
+app.post('/', function (req, res, next) {
+  if (req.headers['x-upload-key'] !== config.uploadKey) return next(403);
+  _.each(req.files, function (fileInfo) {
+    gm(fileInfo.path).format(function (er, ext) {
+      if (er || !ext) return next(er || 500);
+      fs.readFile(fileInfo.path, function (er, file) {
+        if (er) return next(er);
+        var hash = crypto.createHash('md5').update(file).digest('hex');
+        var name = hash + '.' + ext.toLowerCase();
+        s3.put('/' + name, _.extend({
+          'Content-Length': fileInfo.size,
+          'Content-Type': mime.lookup(ext)
+        }, config.headers))
+        .on('response', function () { res.send({file: name}); })
+        .on('error', function (er) { next(er); })
+        .end(file);
+      });
+    });
+  });
+});
+
+// Redirect to hosts specified in config if the UA is bad.
 app.use(function (req, res, next) {
-  var proto = req.protocol;
-  var path = req.path.replace(/\/+/g, '/').replace(/\/$/, '') || '/';
-  var query = req.url.replace(/^[^?]*/, '');
-  var current = proto + '://' + req.host + req.url;
-  var host =
-    ~_.indexOf(config.hosts, req.host) ?
-    req.host :
-    config.hosts[Math.floor(Math.random() * config.hosts.length)];
-  var correct = (proto + '://' + host + path).toLowerCase() + query;
-  if (current === correct) return next();
-  res.redirect(correct, 301);
+  var ua = req.headers['user-agent'];
+  if (!ua || ua === 'Amazon CloudFront') return next();
+  var host = config.hosts[Math.floor(Math.random() * config.hosts.length)];
+  res.redirect(req.protocol + '://' + host + req.url, 301);
 });
 
 // Requests should be in the form /path/to/image(-job-name).extension
@@ -64,5 +86,11 @@ app.get(new RegExp(config.pattern), function (req, res, next) {
 // Ensure bad responses aren't cached.
 app.use(function (er, req, res, next) {
   res.set('Cache-Control', 'no-cache');
-  res.send(404, '');
+
+  // Log interesting errors.
+  if (!_.isNumber(er)) console.error(er.stack || er);
+
+  // Send an empty response body.
+  res.send(http.STATUS_CODES[er] ? er : 500, '');
 });
+
